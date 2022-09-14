67897e7b8fd6464dd51e8f8c12844faf636711ef
MatthewMcMarvelous <terminatorlincoln@yahoo.com>
2022-09-13 21:05:15 -0500

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProgrammingViewComponent } from './programming-view/programming-view.component';
+import { AboutViewComponent } from './about-view/about-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProgrammingViewComponent,
+    AboutViewComponent
   ],
   imports: [
     BrowserModule,
